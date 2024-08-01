ad111b374bd6f53b7dfb88b17ac24ddddd3db93a
pudinmah <pudinmah69@gmail.com>
2024-08-01 14:10:20 +0700

    read data

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
